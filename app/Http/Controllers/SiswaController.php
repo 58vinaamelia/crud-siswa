ec1a68278bea9f9c93196cb62b7cb771efcb28f3
58vinaamelia <vinameow011225imut@gmail.com>
2025-08-08 10:45:55 +0700

    membuat show di view dan menampilkan di halaman beranda

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,39 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Clas;
+use App\Models\clas;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
-use App\Http\Controllers\SiswaController;
 
 class SiswaController extends Controller
 {
-  public function index() {
-    $siswas = User::all(); // GANTI ini
-    return view('siswa.index', compact('siswas'));
-
+     // mengarahkan ke halaman index
+    public function index(){
+        //siapkan data siswas
+        $siswas = user::all();
+        return view('siswa.index',compact('siswas'));
     }
+    // mengarahkan ke halaman create
+     public function create(){
 
-    public function create(){
-        $clases = Clas::all();
+        // siapkan data kelas
+        $clases = clas::all();
+
+        //alihkan ke halaman create siswa
         return view('siswa.create', compact('clases'));
-    }
+     }
 
-    //fungsi store data siswa
-    public function store (Request $request) {
-    //validasi data
-    $request->validate([
-        'name'             => 'required',
-        'nisn'             => 'required | unique:users,email',
-        'alamat'           => 'required',
-        'email'            => 'required | unique:users,email',
-        'password'         => 'required',
-        'no_handphone'     => 'required | unique:users,no_handphone',
-    ]);
+     //fungsi store data siswa
+    public function store(Request $request){
+    // validasi data
+       $request->validate([
+        'name'         =>'required',
+        'nisn'         =>'required',
+        'alamat'       =>'required',
+        'email'        =>'required | unique:users,email',
+        'password'     =>'required',
+        'no_handphone' =>'required  | unique:users,email',
+       ]);
 
-    $datasiswa_store =[
-        'clas_id'          => $request->kelas_id,
+     //siapa data yanag akan di masukan
+      $datasiswa_store =[
+        'clas_id'          => $request->kelas,
         'name'             => $request->name,
         'nisn'             => $request->nisn,
         'alamat'           => $request->alamat,
@@ -42,28 +48,39 @@ class SiswaController extends Controller
         'password'         => $request->password,
         'no_handphone'     => $request->no_handphone,
     ];
-
-    $datasiswa_store['photo'] =$request->file('photo')->store('profilesiswa', 'public');
-
-    // masukan data ke tabel user
-    User::create($datasiswa_store);
-
-    //arahkan user ke halaman beranda
-        return redirect('/');
+     $datasiswa_store['photo'] = $request->file('photo')->store('profilesiswa','public');
+       //masukan data ke tabel User
+       User::create($datasiswa_store);
+       //arahkan User ke halaman beranda
+         return redirect('/');
     }
-
-    //fungsi delete
+    //fungsi delete siswa
     public function destroy($id){
-        //cari user didalam database berdasarkan id yang dikirimkan
-        $datauser = user::find($id);
+        // cari user di dalam database berdasarkan id yang di kirimkan
+        $datauser = User::find($id);
 
-        //lakukan delete pada data tersebut jika data user tersebut ada
-        if ($datauser != null){
-           Storage::disk('public')->delete($datauser->photo);
+        //lakukan delete pada data tersebut jika data user berhasil ada
+        if ($datauser !=null){
+            Storage::disk('public')->delete($datauser->photo);
             $datauser->delete();
         }
 
-        //kembalikan user ke halaman beranda / home
+        //kembalikan user kehalaman beranda/home
         return redirect('/');
     }
+    // fungsi untuk detail siswa
+    public function show($id){
+
+        // cari data siswa di dalam tabel user dengan id yang dikirimkan
+        $datauser = User::find($id);
+
+        //cek apakah datanya ada atau tidak
+        if($datauser == null){
+            return redirect('/');
+        }
+
+            //pindahkan user ke halaman detail siswa dengan mengerjakan data detailnya
+        return view('siswa.show',compact('datauser'));
+
+}
 }
